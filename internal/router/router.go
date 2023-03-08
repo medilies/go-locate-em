22fa61c7ee14317870acbdd084e0063ce46351c5
medilies <medilies@gmail.com>
2023-03-08 22:59:33 +0100

    static files server with OSM leaflet page

--- internal/router/router.go
+++ internal/router/router.go
@@ -11,5 +11,8 @@ type Router struct{}
 var hc = &controllers.HomeController{}
 
 func (Router) RegisterRoutes(mux *http.ServeMux) {
-	mux.HandleFunc("/", hc.Index)
+	mux.HandleFunc("/search", hc.Index)
+
+	fs := http.FileServer(http.Dir("static"))
+	mux.Handle("/public/", http.StripPrefix("/public/", fs))
 }
